c4381ce61aafe9c5049ed7eb1692eee463a16993
Manuel Cattelan <cattelanmanuel01@gmail.com>
2025-03-31 18:07:15 +0200

    feat(server): working dockerfile

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 
-from . import app
+from . import routes
 from . import constants
 
 
 def create_app():
     flask = Flask(__name__)
-    flask.register_blueprint(app.blueprint)
+    flask.register_blueprint(routes.blueprint)
 
     flask.config['ATTACHMENTS_FOLDER'] = constants.ATTACHMENTS_FOLDER
     flask.config['MAX_CONTENT_LENGTH'] = constants.MAX_CONTENT_LENGTH
